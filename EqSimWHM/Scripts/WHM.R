9171df2fc0591fa1d69c93c72789ce2f66473b36
Andy Campbell <andrew.campbell@marine.ie>
2020-06-10 09:34:35 +0100

    Some graphical updates

--- EqSimWHM/Scripts/WHM.R
+++ EqSimWHM/Scripts/WHM.R
@@ -6,38 +6,76 @@ try(dev.off(),silent=TRUE)
 try(sink(),silent=TRUE)
 
 library(FLCore)
-library(Cairo)
+library(Cairo)    #plotting
+library(devEMF)   
+library(officer)  #for op to word doc
 library(gplots)   #rich.colors
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+sessionInfo()
+#R version 3.5.3 (2019-03-11)
+#Platform: x86_64-w64-mingw32/x64 (64-bit)
+#Running under: Windows 10 x64 (build 17763)
+
+#Matrix products: default
+
+#locale:
+#[1] LC_COLLATE=English_Ireland.1252  LC_CTYPE=English_Ireland.1252    LC_MONETARY=English_Ireland.1252
+#[4] LC_NUMERIC=C                     LC_TIME=English_Ireland.1252    
+
+#attached base packages:
+#[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+#[1] ggplot2_3.3.1    tidyr_1.0.2      dplyr_0.8.5      gplots_3.0.3     Cairo_1.5-12     FLCore_2.6.14    iterators_1.0.12
+#[8] lattice_0.20-38 
+
+#loaded via a namespace (and not attached):
+#[1] Rcpp_1.0.3         pillar_1.4.3       compiler_3.5.3     bitops_1.0-6       tools_3.5.3        packrat_0.5.0     
+#[7] lifecycle_0.2.0    tibble_3.0.1       gtable_0.3.0       pkgconfig_2.0.3    rlang_0.4.5        Matrix_1.2-18     
+#[13] rstudioapi_0.11    withr_2.1.2        vctrs_0.2.4        gtools_3.8.1       caTools_1.17.1.2   hms_0.5.3         
+#[19] stats4_3.5.3       grid_3.5.3         tidyselect_1.0.0   glue_1.4.0         R6_2.4.1           gdata_2.18.0      
+#[25] readr_1.3.1        purrr_0.3.3        magrittr_1.5       scales_1.1.0       ellipsis_0.3.0     MASS_7.3-51.5     
+#[31] assertthat_0.2.1   colorspace_1.4-1   KernSmooth_2.23-16 munsell_0.5.0      crayon_1.3.4  
+
+
 #locations
 Drive <- "C:"
 Base.dir <- file.path(Drive,"Stocks","hom_27_2a4a5b6a7a-ce-k8")
+#historic assessments here (not required)
 Assessment.Dir <- file.path(Base.dir,"Assessment")
 
+#moved code to wk_WKREBUILD location 08/06/2020
 #MSE.dir <- file.path(Base.dir,"MP_MSE","MSE 2019","whm.MSE2019.WKREBUILD")
-MSE.dir <- file.path(Base.dir,"MP_MSE","wk_WKREBUILD","RqSimWHM")
+MSE.dir <- file.path(Base.dir,"MP_MSE","wk_WKREBUILD","EqSimWHM")
 
-Data.dir <- file.path(MSE.dir,"Data")              #this is where the results of the 1000 assessment runs for initialisation of the MSE are saved
-RData.dir <- file.path(MSE.dir,"RData")            #historic assessment outputs, stock-recruit fits
-Log.dir <- file.path(MSE.dir,"Logs")              #debug/verbose output
+#this is where the results of the 1000 assessment runs for initialisation of the MSE are saved
+Data.dir <- file.path(MSE.dir,"Data")              
+#any useful stuff contained in RData files
+RData.dir <- file.path(MSE.dir,"RData")
+#debug/verbose output
+Log.dir <- file.path(MSE.dir,"Logs")              
+#Simulation and statistical outputs
 Res.dir <- file.path(MSE.dir, "Results")
 
-Source.dir <- file.path(Base.dir,"R")              #R functions
-Scripts.dir <- file.path(Base.dir, "Scripts")      #R scripts
+Source.dir <- file.path(MSE.dir,"R")              #R functions
+Scripts.dir <- file.path(MSE.dir, "Scripts")      #R scripts
 
 #OMs, MPs
 source(file = file.path(Scripts.dir,"OMs.R"))
 source(file = file.path(Scripts.dir,"MPs.R"))
 
+#source functions
+sapply(list.files(path=file.path(Source.dir), pattern=".R", full.names=TRUE), source)
+
+#basic simulation settings
 #niters <- 10000
 niters <- 1000
 nyr <- 50
 
-#source functions
-sapply(list.files(path=file.path(Source.dir), pattern=".R", full.names=TRUE), source)
+
 
 #OM <- OM2; MP <- MP2.0_10000
 #OM <- OM2; MP <- MP3.0
@@ -88,10 +126,29 @@ gCW <- ggplot(data = dfWeights, mapping = aes(x=Year,y=CW)) +
   geom_hline(data = dfSAWeights, mapping=aes(yintercept=CW), col="red") + 
   facet_wrap(~Age) + ylab("Catch Weight")
 
+emf(file = file.path(res.Dir,"CW.emf"), width = 7, height = 7)
+print(gCW)
+dev.off()
+
 gSW <- ggplot(data = dfWeights, mapping = aes(x=Year,y=SW)) +
   geom_line(aes(group=Age)) +
   geom_hline(data = dfSAWeights, mapping=aes(yintercept=SW), col="red") + 
   facet_wrap(~Age) + ylab("Stock Weight")
+
+emf(file = file.path(res.Dir,"SW.emf"), width = 7, height = 7)
+print(gSW)
+dev.off()
+
+read_docx() %>%
+  body_add("Catch Weights") %>%
+  body_add_img(src = file.path(Res.dir,"CW.emf"), width = 7, height = 7) %>% 
+  body_add_break() %>%
+  body_add("Stock Weights") %>%
+  body_add_img(src = file.path(Res.dir,"SW.emf"), width = 7, height = 7) %>% 
+  print(target = file.path(Res.dir,"HistWeights.docx"))
+  
+file.remove(file.path(res.Dir,"CW.emf"))
+file.remove(file.path(res.Dir,"SW.emf"))
 
 #assign the stock and catch weights into the assessment FLStock object
 tSW <- FLQuant(t(dfSW[,-1]), dim=dim(stock.wt(WHOM.WGWIDE2019)), dimnames=dimnames(stock.wt(WHOM.WGWIDE2019)))
