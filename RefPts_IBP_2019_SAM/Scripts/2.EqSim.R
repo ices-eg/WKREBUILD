720baa3284833d856cd18344d64d2ec379f1e1e6
Martin Pastoors <martin.pastoors@gmail.com>
2020-06-17 17:33:04 +0200

    Include all the relevant bits in one code

--- RefPts_IBP_2019_SAM/Scripts/2.EqSim.R
+++ RefPts_IBP_2019_SAM/Scripts/2.EqSim.R
@@ -16,6 +16,55 @@ library(FLCore)
 library(knitr)
 library(plyr)
 
+ac <- function(str){as.character(str)}
+fmt <- function(RP,dgt=3,fmt="f"){ac(formatC(RP,digits=dgt,format=fmt,big.mark = ","))}
+roundUp <- function(x,to=1000) {to*(x%/%to + as.logical(x%%to))}
+
+subfolder <- "RefPts_IBP_2019_SAM"
+RData.dir <- file.path(getwd(),subfolder, "RData")
+graphics.dir <- file.path(getwd(),subfolder, "Graphics")
+logs.dir <- file.path(getwd(),subfolder, "Logs")
+source.dir <- file.path(getwd(),subfolder, "Source")
+
+#WGWIDE2018
+WG18 <- get(load(file=file.path(RData.dir,"WGWIDE2018.RData")))
+name(WG18) <- "WGWIDE18"
+WG18df <-
+  as.data.frame(WG18) %>% 
+  bind_rows(as.data.frame(fbar(WG18)) %>% mutate(slot="fbar")) %>% 
+  bind_rows(as.data.frame(ssb(WG18)) %>% mutate(slot="ssb")) %>% 
+  mutate(run="WG18")
+
+#WGWIDE2019
+WG19 <- get(load(file=file.path(RData.dir,"WGWIDE2019.RData")))
+name(WG19) <- "WGWIDE19"
+WG19df <-
+  as.data.frame(WG19) %>% 
+  bind_rows(as.data.frame(fbar(WG19)) %>% mutate(slot="fbar")) %>% 
+  bind_rows(as.data.frame(ssb(WG19)) %>% mutate(slot="ssb")) %>% 
+  mutate(run="WG19")
+
+#WGWIDE2019SAM
+
+WG19SAM <- get(load(file=file.path(RData.dir,"WGWIDE2019SAM.RData")))
+name(WG19SAM) <- "WGWIDE19SAM"
+
+WG19SAMdf <-
+  as.data.frame(WG19SAM) %>% 
+  bind_rows(as.data.frame(fbar(WG19SAM)) %>% mutate(slot="fbar")) %>% 
+  bind_rows(as.data.frame(ssb(WG19SAM)) %>% mutate(slot="ssb")) %>% 
+  mutate(run="WG19SAM")
+
+
+#plot
+# bind_rows(WG18df, WG19df, WG19SAMdf) %>% 
+#   filter(slot %in% c("ssb","fbar")) %>% 
+#   
+#   ggplot(aes(x=year,y=data)) +
+#   theme_bw() +
+#   geom_line(aes(colour=run)) +
+#   facet_wrap(~slot, scales="free_y")
+
 # each scenario was run without the 1982 data point
 # source(file=file.path(getwd(),"Scripts","0.Setup.R"))
 
@@ -44,20 +93,20 @@ dfResults <- data.frame("Assessment" = character(),
                         "RunTime" = character(),
                         stringsAsFactors = FALSE)
 
-rt <- format(Sys.time(), "%d-%b-%Y %H.%M")
-
-
 #fs to scan
 #Fscan <- seq(0.01,0.2,len=20) #low res
 Fscan <- seq(0.01,0.2,len=40) #high res
 
 #Base assessments
 ass <- c("WG18")
-ass <- c("WG18","WG19","WG19SAM")
+ass <- c("WG19","WG19SAM")
+# ass <- c("WG18","WG19","WG19SAM")
 
-a <- "WG18"
+# a <- "WG18"
 
 for (a in ass) {
+
+  rt <- format(Sys.time(), "%d-%b-%Y %H.%M")
   
   cat("EqSim analysis based on output from",a,"assessment","\n**************************************************\n")
   
@@ -168,7 +217,7 @@ for (a in ass) {
                             rhologRec = rhologRec,
                             Blim = Blim, Bpa = Bpa,
                             recruitment.trim = recruitment.trim,
-                            Btrigger = MSYBtrigger5,
+                            Btrigger = MSYBtrigger,
                             verbose = TRUE)
   
   # eqsim_plot(SIM2.Fmsy,catch=TRUE)
@@ -199,13 +248,19 @@ for (a in ass) {
 
 } #loop over assessments
 
-save(dfResults, file = file.path(RData.dir, "dfResults.RData"))
+# save(dfResults, file = file.path(RData.dir, "dfResults.RData"))
 
 # overview
 
+dfResults <- bind_rows(dfResults_wg18, dfResults)
+
 dfResults %>% 
+  group_by(Assessment, RP) %>% 
+  filter(row_number() == 1) %>% 
   filter(RP %in% c("Blim","MSYBtrigger","Flim","Fpa", "FMSY","FMSY_final","FP05")) %>% 
   dplyr::select(Assessment, RP, Val) %>% 
   pivot_wider(values_from = Val, names_from =Assessment) %>% 
   pander::pandoc.table()
 
+
+# dfResults_wg18 <- dfResults
