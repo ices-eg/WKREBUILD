c65ffdb940ccfcd539909868a3cd97aa39a5b7f2
Martin Pastoors <martin.pastoors@gmail.com>
2020-06-23 12:09:09 +0200

    Updates on MP and OMs

--- EqSimWHM/Scripts/OMs.R
+++ EqSimWHM/Scripts/OMs.R
@@ -44,3 +44,15 @@ OM2.2 <- list("code" = "OM2.2",
             refPts = list("Fpa" = 0.074, "Flim" = 0.103, "Fmsy" = 0.074, "Bpa" = 1168272,
                           "Blim" = 834480, "MSYBtrigger" = 1168272, "Bloss" = 761613),
             "pBlim" = 0.05)
+
+#WGWIDE2019 SAM assessment, IBPWHM method for reference points, stochastic bio and selection
+OM2.2sam <- list("code" = "OM2.2_sam",
+              "desc" = "WGWIDE19_sam",
+              "IM" = NA,
+              "SRR" = "SRR.WG19.SegReg_Blim.exterm", "RecAR" = TRUE, maxRecRes = c(3,-3),
+              "BioYrs" = c(2008,2017), "BioConst" = FALSE, 
+              "SelYrs" = c(2008,2017), "SelConst" = FALSE,
+              "Obs" = NA,
+              refPts = list("Fpa" = 0.115, "Flim" = 0.161, "Fmsy" = 0.115, "Bpa" = 856540,
+                            "Blim" = 611814, "MSYBtrigger" = 856540, "Bloss" = 604476),
+              "pBlim" = 0.05)
