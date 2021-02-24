3a5c386d85418855034087956229bb06b7d49a75
Martin <martin.pastoors@gmail.com>
2021-02-24 14:35:32 +0100

    Updates to SAM simulator scripts and utils

--- EqSimWHM/Scripts/00_SAM simulator.r
+++ EqSimWHM/Scripts/00_SAM simulator.r
@@ -5,17 +5,24 @@
 rm(list=ls())
 gc()
 
-# install.packages("devtools")
-
 # install.packages("FLCore", repos="http://flr-project.org/R")
 library(FLCore)
+
+# install.packages("ggplotFL", repos="http://flr-project.org/R")
+library(ggplotFL)
+
+library(TMB)
 
 # devtools::install_github('fishfollower/SAM/stockassessment') 
 library(stockassessment)
 
 library(tidyverse)
 
-basedir  <- "E:/MARTIN"
+source("D:/GIT/wk_WKREBUILD/EqSimWHM/R/sam_fit2.r")
+source("D:/GIT/wk_WKREBUILD/EqSimWHM/R/utilities.r")
+
+# basedir  <- "E:/MARTIN"
+basedir  <- "C:/TEMP"
 sao.name <- "WHOM_2019"
 tempdir  <- file.path(basedir,sao.name) 
 
@@ -50,7 +57,16 @@ dat<-setup.sam.data(surveys=surveys,
 
 conf <- loadConf(dat,"conf/model.cfg", patch=TRUE)
 par  <- defpar(dat, conf)
-fit  <- sam.fit(dat, conf, par)
+
+print("running initial SAM")
+fit  <- sam.fit2(dat, conf, par, silent=TRUE)
+
+# # should be 0 if the model has converged
+# fit$opt$convergence 
+# # You can extract the standard errors of the fixed parameters with:
+# all(!is.na(unlist(fit$plsd[which(names(fit$plsd)%in%names(fit$sdrep$par.fixed))])))
+# # The max gradient should be closed to 0, the threshold is a bit subjective but < 1e-4 is already good:
+# max(fit$sdrep$gradient.fixed) 
 
 # read input in VPA file format
 inputs <- c(landings.fraction='lf.dat', catch.n='cn.dat', catch.wt='cw.dat',
@@ -121,30 +137,32 @@ units(FLS@harvest)   <-  "f"
 # ssb
 FLS@stock            <- ssb(FLS)
 
-validObject(FLS)
-
+# validObject(FLS)
 # plot(FLS)
 # plot(FLS@catch)
 
 # set simulator properties
-nsim     <- 1000
+nsim     <- 10
 set.seed(123)
 
 # Generate FLstock object with iterations
 FLSs <- propagate(FLS, nsim)
 
 # Simulate the observations
-simdata <- simulate(fit, nsim=nsim,  full.data=TRUE)
+simdata <- simulate(fit, nsim=1.2*nsim,  full.data=TRUE)
+simruns <- data.frame(stringsAsFactors = FALSE)
 
 # iterations. start with 2, 1 is the real observation.
-i <- 708   
+i  <- 2
+it <- 2
 
 while (i <= nsim) {
   
   start_time <- Sys.time()
   
-  # sim_fit <-  invisible(capture.output(sam.fit(simdata[[i]], conf, par)))
- sim_fit <-  sam.fit(simdata[[i]], conf, par)
+  # sim_fit <-  sam.fit(simdata[[it]], conf, par)
+  # sim_fit  <- sam.fit2(simdata[[it]], conf, par, silent=TRUE)
+  sim_fit  <- try(sam.fit2(simdata[[it]], conf, par, silent=TRUE), TRUE)
   
   if(exists("sim_fit")) {
     
@@ -166,25 +184,45 @@ while (i <= nsim) {
     FLSs[,,,,,i]@stock            <- ssb(FLSs[,,,,,i])
     
     save(FLSs, file="run/FLSs.RData")
-    rm(sim_fit)
     
     end_time <- Sys.time()
-    duration = end_time - start_time
-    print(paste("run", i, duration, sep=" "))
+    duration <- end_time - start_time
+    df       <- base::data.frame(
+      assess    = sao.name,
+      run       = i,
+      starttime = start_time,
+      endtime   = end_time,
+      duration  = round(end_time - start_time, digits=0),
+      aic       = as.numeric(AIC(sim_fit)),
+      convergence = sim_fit$opt$convergence,
+      allSDnotNA  = all(!is.na(unlist(fit$plsd[which(names(fit$plsd)%in%names(fit$sdrep$par.fixed))]))),
+      maxgradient = max(fit$sdrep$gradient.fixed),
+      stringsAsFactors = FALSE)
+    simruns <- bind_rows(simruns, df)
+    save(simruns, file="run/simruns.RData")
+    
+    d <- (nsim-i) * as.numeric(mean(simruns$duration))/60
+
+    print(paste("run", i, " of ", nsim, round(as.numeric(duration), digits=0), 
+                "sec;", round(d, digits=2), "minutes remaining", sep=" "))
+    
+    rm(sim_fit)
+    invisible(gc())
     
     i = i + 1
-    
-    
-    
-  }
+  } # end of if statement
+  
+  # increase i trials in all cases
+  it = it + 1
+  
 } # end of while statement
 
 
-# load(file="C:/TEMP/WHOM_2020/run/FLSs.RData")
+df2019 <- as.data.frame(loadRData(file="C:/TEMP/WHOM_2019/run/FLSs.RData"))  %>%  mutate(assess="SAM2019")
+df2020 <- as.data.frame(loadRData(file="C:/TEMP/WHOM_2020/run/FLSs.RData"))  %>%  mutate(assess="SAM2020")
 
 df <-
-  as.data.frame(FLSs) %>% 
-  
+  bind_rows(df2019, df2020) %>% 
   filter(slot=="harvest", age >= minage, age <= maxage) %>%
   group_by(year, unit, season, area, iter) %>% 
   summarise(data = mean(data, na.rm=TRUE)) %>% 
@@ -192,7 +230,17 @@ df <-
     slot="meanf",
     age ="1-10"
   ) %>% 
-  bind_rows(as.data.frame(FLSs))
+  bind_rows(df2019, df2020)
+
+
+
+df %>% 
+  filter(slot=="stock") %>% 
+  ggplot(aes(year, data, group=iter)) +
+  theme(legend.position="none") +
+  geom_line(aes(colour=assess)) +
+  geom_line(data=filter(df, slot=="stock", iter==1), colour="black", size=1) +
+  facet_wrap(~assess)
 
   
     
